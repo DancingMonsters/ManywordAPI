1fd12a5dda2293b81dcedbf675a1fec4b3b762a7
Vladislav Goloushkin <errannnnnnn@gmail.com>
2021-05-08 16:24:55 +0800

    add alphabets and weights

---
+++ app/Http/Controllers/AlphabetsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\alphabets;
+
+class AlphabetsController extends Controller
+{
+  public function add($language, Request $req) {
+      $alphabet = new alphabets();
+      $alphabet -> alphabet = $req -> input('alphabet');
+      $alphabet -> save();
+  }
+
+  public function get($language) {
+      $alphabet = alphabets::where('lang', $language) -> get() -> first();
+      return response()->json($alphabet -> alphabet);
+  }
+}
---
+++ app/Http/Controllers/WeightsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\weights;
+
+class WeightsController extends Controller
+{
+  public function add($language, Request $req) {
+      $weights = new weights();
+      $weights -> weights = $req -> input('weights');
+      $weights -> save();
+  }
+
+  public function get($language) {
+      $weights = weights::where('lang', $language) -> get() -> first();
+      return response()->json($weights -> weights);
+  }
+}
