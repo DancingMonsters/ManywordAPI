449299e6422cadad53723afe4925f682870015dc
ERRAN <mrvlhaka@gmail.com>
2021-09-11 09:28:46 +0800

    dictionaryController

--- app/Http/Controllers/dictionaryController.php
+++ app/Http/Controllers/dictionaryController.php
@@ -23,11 +23,61 @@ class dictionaryController extends Controller
             return response()->json(["status" => false]);
     }
 
-    public function index($language) {
-        if($words = $this->getDictionaryTable($language))
-            return response()->json(["status" => true, "words" => $words -> get()]);
-        else
-            return response()->json(["status" => false]);
+    public function index(?Request $request, $language) {
+        if($request->filled('page'))
+        {
+            $page = (int)$request->query('page', 1);
+            $size = (int)$request->query('size', 20);
+            $search = $request->query('search', null);
+            return response()->json($this->getPage($language, $page, $size, $search));
+        } else {
+            if($words = $this->getDictionaryTable($language))
+                return response()->json(["status" => true, "words" => $words -> get()]);
+            else
+                return response()->json(["status" => false]);
+        }
+    }
+
+    public function getPagesCount(string $language, int $size, array $search = null) {
+        $query = $this->getDictionaryTable($language);
+        if ($search !== null)
+        {
+            foreach ($search as $key => $item)
+            {
+                if($key == 'word')
+                {
+                    $query = $query->where('word', 'LIKE', '%' . $item . '%');
+                } else {
+                    $query = $query->where('particle', '=', $key);
+                }
+            }
+        }
+        $wordsCount = $query->get()->count();
+        $pages = intdiv($wordsCount, $size);
+        $pages += ($wordsCount % $size) == 0 ? 0 : 1;
+        return $pages;
+    }
+
+    private function getPage(string $language, int $page, int $size, array $search = null)
+    {
+        $query = $this->getDictionaryTable($language)
+            ->select('*');
+        if($search !== null)
+        {
+            dd($search);
+            foreach ($search as $key => $item)
+            {
+                if($key == 'word')
+                {
+                    $query = $query->where('word', 'LIKE', '%' . $item . '%');
+                } else {
+                    $query = $query->where('particle', '=', $item);
+                }
+            }
+        }
+        return $query
+            ->skip(($page-1) * $size)->take($size)
+            ->get()->toArray();
     }
 
     public function create(Request $req, $language) {
