4dcf5d8bcf11a05d173016321d5722d23766770e
ERRAN <errannnnnnn@gmail.com>
2022-02-05 15:41:09 +0800

    DictionaryController.php добавить слово

--- app/Modules/Dictionary/DictionaryController.php
+++ app/Modules/Dictionary/DictionaryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Dictionary;
 
+use App\Modules\Dictionary\FormServices\DictionaryFormService;
 use App\Modules\Dictionary\Services\DictionaryService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -15,5 +16,20 @@ class DictionaryController
     public function get(Request $request): JsonResponse
     {
         return response()->json((new DictionaryService())->get($request));
+    }
+
+    /**
+     * Добавление слова
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createWord(Request $request): JsonResponse
+    {
+        $formService = new DictionaryFormService($request);
+        if (!$formService->validate()) {
+            return response()->json(['error' => 1, 'messages' => $formService->messages]);
+        } else {
+            return response()->json((new DictionaryService())->createWord($request));
+        }
     }
 }
