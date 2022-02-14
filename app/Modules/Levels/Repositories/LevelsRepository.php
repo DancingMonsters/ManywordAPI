57e64705bb1889764ae79952de74e3a96ca33d71
ERRAN <errannnnnnn@gmail.com>
2022-02-15 01:51:10 +0800

    LevelsRepository.php поправил получение уровней

--- app/Modules/Levels/Repositories/LevelsRepository.php
+++ app/Modules/Levels/Repositories/LevelsRepository.php
@@ -12,18 +12,24 @@ class LevelsRepository
     /**
      * Получение уровней
      * @param array $fields
-     * @param int $language
-     * @param int $historyID
-     * @param int $published
+     * @param int|null $language
+     * @param int|null $historyID
+     * @param bool $published
      * @return Collection
      */
-    public function get(array $fields, int $language, int $historyID, int $published): Collection
+    public function get(array $fields, int $language = null, int $historyID = null, bool $published = null): Collection
     {
         return DB::table('levels')
             ->select($fields)
-            ->where('history_id', '=', $historyID)
-            ->where('language', '=', $language)
-            ->where('published', '=', $published)
+            ->when($language !== null, function ($query) use ($language) {
+                $query->where('language', '=', $language);
+            })
+            ->when($historyID !== null, function ($query) use ($historyID) {
+                $query->where('history_id', '=', $historyID);
+            })
+            ->when($published !== null, function ($query) use ($published) {
+                $query->where('published', '=', $published);
+            })
             ->get();
     }
 
