70945a2add82337aa0926460bfb815a7a3b49b84
ERRAN <errannnnnnn@gmail.com>
2022-03-12 03:51:52 +0800

    api.php роут для обновления уровня

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::prefix('histories')->group(function() {
 Route::prefix('levels')->group(function () {
     Route::get('/', [LevelsController::class, 'get']);
     Route::get('{id}', [LevelsController::class, 'getById'])->where('id', '[0-9]+');
+    Route::patch('{id}', [LevelsController::class, 'setById'])->where('id', '[0-9]+');
 });
 
 Route::prefix('alphabet')->group(function () {
