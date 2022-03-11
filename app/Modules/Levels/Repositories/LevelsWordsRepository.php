15f24371bbbd4c85afd14053f5710217319dc8dd
ERRAN <errannnnnnn@gmail.com>
2022-03-12 03:26:54 +0800

    LevelsWordsRepository.php удаление и добавление слов в уровень

--- app/Modules/Levels/Repositories/LevelsWordsRepository.php
+++ app/Modules/Levels/Repositories/LevelsWordsRepository.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class LevelsWordsRepository
 {
+    private string $table = 'levels_words';
+
     /**
      * Получение слов уровня
      * @param int $levelId
@@ -14,10 +16,34 @@ class LevelsWordsRepository
      */
     public function getByLevelId(int $levelId): Collection
     {
-        return DB::table('levels_words')
+        return DB::table($this->table)
             ->select('words.*')
-            ->leftJoin('words', 'words.id', '=', 'levels_words.word_id')
-            ->where('levels_words.level_id', '=', $levelId)
+            ->leftJoin('words', 'words.id', '=', $this->table . '.word_id')
+            ->where($this->table . '.level_id', '=', $levelId)
             ->get();
+    }
+
+    /**
+     * Добавление слова по id уровня
+     * @param int $levelId
+     * @param int $wordId
+     */
+    public function addByLevelId(int $levelId, int $wordId)
+    {
+        DB::table($this->table)
+            ->insert(['level_id' => $levelId, 'word_id' => $wordId]);
+    }
+
+    /**
+     * Удаление слова по id уровня и слова
+     * @param int $levelId
+     * @param int $wordId
+     */
+    public function deleteByLevelId(int $levelId, int $wordId)
+    {
+        DB::table($this->table)
+            ->where('word_id', '=', $wordId)
+            ->where('level_id', '=', $levelId)
+            ->delete();
     }
 }
