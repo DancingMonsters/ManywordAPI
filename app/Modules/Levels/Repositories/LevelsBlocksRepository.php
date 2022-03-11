b2dc2c5cb3721e3ed26b33bdd61f1db524a4d673
ERRAN <errannnnnnn@gmail.com>
2022-03-12 03:04:39 +0800

    LevelsBlocksRepository.php обновление блоков уровня

--- app/Modules/Levels/Repositories/LevelsBlocksRepository.php
+++ app/Modules/Levels/Repositories/LevelsBlocksRepository.php
@@ -6,10 +6,27 @@ use Illuminate\Support\Facades\DB;
 
 class LevelsBlocksRepository
 {
+    /**
+     * Получение блоков уровня по его id
+     * @param int $levelID
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Query\Builder|object|null
+     */
     public function getBlocksByLevelID(int $levelID) {
         return DB::table('levels_blocks')
-            ->select('blocks')
+            ->select('blocks', 'id')
             ->where('level_id', '=', $levelID)
             ->first();
+    }
+
+    /**
+     * Обновление блоков уровня
+     * @param int $blocksID
+     * @param string $blocks
+     */
+    public function updateBlocksByID(int $blocksID, string $blocks)
+    {
+        DB::table('levels_blocks')
+            ->where('id', '=', $blocksID)
+            ->update(['blocks' => $blocks]);
     }
 }
